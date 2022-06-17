4e04dbeb8906c9a0f0df924d7067c100fcabe588
avandefeniks <angel.vandefeniks@gmail.com>
2022-06-16 18:55:56 -0700

    completed add employee and add role

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const { resolve } = require('path');
+// const { resolve } = require('path');
 // const consoleTable = require('console.table');
 require('dotenv').config();
 
@@ -83,6 +83,11 @@ const firstAction = firstActionData => {
             if (firstActionData.firstChoice === 'Add Employee') {
                 addEmployee();
             }
+
+            // update role
+            if (firstActionData.firstChoice === 'Update Employee Role') {
+                updateRole();
+            }
         })
 }
 
@@ -91,7 +96,16 @@ const addDepartment = addDepartmentData => {
         {
             type: 'input',
             name: 'department',
-            message: 'What is the name of the new department?'
+            message: 'What is the name of the new department?',
+            validate: departmentInput => {
+                if (departmentInput) {
+                    return true;
+                }
+                else {
+                    console.log("Please enter a department!");
+                    return false;
+                }
+            }
         }
     ])
         .then(addDepartmentData => {
@@ -116,12 +130,30 @@ const addRole = addRoleData => {
             {
                 type: 'input',
                 name: 'title',
-                message: 'Please enter a role'
+                message: 'Please enter a role',
+                validate: titleInput => {
+                    if (titleInput) {
+                        return true;
+                    }
+                    else {
+                        console.log("Please enter a new role!");
+                        return false;
+                    }
+                }
             },
             {
                 type: 'input',
                 name: 'salary',
-                message: 'Please enter a salary'
+                message: 'Please enter a salary',
+                validate: salaryInput => {
+                    if (salaryInput) {
+                        return true;
+                    }
+                    else {
+                        console.log("Please enter a salary for the role!");
+                        return false;
+                    }
+                }
             },
             {
                 type: 'list',
@@ -152,107 +184,140 @@ const addRole = addRoleData => {
 
 }
 
-// const addEmployee = addEmployeeData => {
-//     const sql = `SELECT id, title AS name FROM role`;
-
-//     db.query(sql, (err, roles) => {
-//         if (err) {
-//             console.log(err);
-//             return;
-//         }
-//         return inquirer.prompt([
-//             {
-//                 type: 'input',
-//                 name: 'firstName',
-//                 message: "What is the employee's fist name?"
-//             },
-//             {
-//                 type: 'input',
-//                 name: 'lastName',
-//                 message: "What is the employee's last name?"
-//             },
-//             {
-//                 type: 'list',
-//                 name: 'role',
-//                 message: "What is the employee's role?",
-//                 choices: roles
-//             }
-
-//         ])
-//     })
-//         .then(getIndexes => {
-//             const sql2 = `SELECT id, CONCAT(first_name, " ", last_name) AS name FROM employee`;
-//             db.query(sql2, (err, employeeList) => {
-//                 if (err) {
-//                     console.log(err);
-//                     return;
-//                 }
-//                 return inquirer.prompt([
-//                     {
-//                         type: 'list',
-//                         name: 'manager',
-//                         message: "Who is the employee's manager?",
-//                         choices: employeeList
-//                     }
-//                 ])
-//             })
-//             const roleIndex = roles.filter(name => addEmployeeData.role === name.name)[0];
-//             console.log(roleIndex.id);
-
-//             const empIndex = employeeList.filter(name => addEmployeeData.manager === name.name)[0];
-//             console.log(empIndex.id);
-//         })
-// }
-
 const addEmployee = addEmployeeData => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'firstName',
-            message: "What is the employee's fist name?"
-        },
-        {
-            type: 'input',
-            name: 'lastName',
-            message: "What is the employee's last name?"
+    const sql = `SELECT id, title AS name FROM role`;
+    const sql2 = `SELECT id, CONCAT(first_name, " ", last_name) AS name FROM employee`;
+
+    db.query(sql, (err, roles) => {
+        if (err) {
+            console.log(err);
+            return;
         }
-    ])
-        .then(roleData => {
-            const sql = `SELECT id, title AS name FROM role`;
-            db.query(sql, (err, roles) => {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
-                return inquirer.prompt([
-                    {
-                        type: 'list',
-                        name: 'role',
-                        message: "What is the employee's role?",
-                        choices: roles
+        db.query(sql2, (err, employeeList) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            return inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'firstName',
+                    message: "What is the employee's fist name?",
+                    validate: firstNameInput => {
+                        if (firstNameInput) {
+                            return true;
+                        }
+                        else {
+                            console.log("Please enter the employee's fist name!");
+                            return false;
+                        }
                     }
-                ])
-            })
-        })
-        .then(empData => {
-            const sql = `SELECT id, CONCAT(first_name, " ", last_name) AS name FROM employee`;
-            db.query(sql, (err, employeeList) => {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
-                return inquirer.prompt([
-                    {
-                        type: 'list',
-                        name: 'employees',
-                        message: "Who is the employee's manager?",
-                        choices: employeeList
+                },
+                {
+                    type: 'input',
+                    name: 'lastName',
+                    message: "What is the employee's last name?",
+                    validate: lastNameInput => {
+                        if (lastNameInput) {
+                            return true;
+                        }
+                        else {
+                            console.log("Please enter the employee's last name!");
+                            return false;
+                        }
                     }
-                ])
-            })
+                },
+                {
+                    type: 'list',
+                    name: 'role',
+                    message: "What is the employee's role?",
+                    choices: roles
+                },
+                {
+                    type: 'list',
+                    name: 'manager',
+                    message: "Who is the employee's manager?",
+                    choices: ['None', ...employeeList]
+                }
+            ])
+
+                .then(addEmployeeData => {
+                    const roleIndex = roles.filter(name => addEmployeeData.role === name.name)[0];
+
+                    const empIndex = employeeList.filter(name => addEmployeeData.manager === name.name)[0];
+                  
+                    // if (addEmployeeData.manager === 'None') {
+                    //     empIndex = null;
+                    // }
+                    //     empIndex;
+                    // console.log(empIndex);
+
+                    db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                    VALUES (?,?,?,?)`, [addEmployeeData.firstName, addEmployeeData.lastName, roleIndex.id, empIndex.id], (err, data) => {
+                        if (err) {
+                            console.log(err);
+                            return;
+                        }
+                    })
+                    console.log(addEmployeeData.firstName + ' ' + addEmployeeData.lastName + ' successfully added to the database');
+                    firstAction()
+                });
+
         })
+    })
+
 }
 
-// firstAction()
+const updateRole = roleData => {
+    const sql = `SELECT id, CONCAT(first_name, " ", last_name) AS name FROM employee`;
+    const sql2 = `SELECT id, title AS name FROM role`;
+
+    db.query(sql, (err, employeeList) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        db.query(sql2, (err, roles) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            return inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'employee',
+                    message: 'What employee would you like to update?',
+                    choices: employeeList
+                },
+                {
+                    type: 'list',
+                    name: 'role',
+                    message: "What is the employee's new role",
+                    choices: roles
+                },
+                
+            ])
+            .then(roleData => {
+                const empIndex = employeeList.filter(name => roleData.employee === name.name)[0];
+                // console.log(empIndex.id);
+
+                const roleIndex = roles.filter(name => roleData.role === name.name)[0];
+                // console.log(roleIndex.id);
+
+                db.query(`UPDATE employee SET role_id = ? WHERE id = ?`, [roleIndex.id, empIndex.id], (err, data) => {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                })
+                console.log("The employee's role has been updated");
+                firstAction()
+            })
+        })
+    })
+}
+
+firstAction()
 // addRole()
-addEmployee()
+// addEmployee()
+// updateRole()
